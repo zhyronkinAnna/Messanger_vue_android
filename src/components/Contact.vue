46bc7eb05ba9194ad8b63fa5d304ac1097f52915
Comp <Comp@DESKTOP-3S08QG9>
2024-08-08 00:28:05 +0300

    add dropdown for contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,15 +1,49 @@
 <script setup lang="ts">
-import { NGridItem, NGrid, NAvatar, NText, NFlex } from 'naive-ui';
+import { NGridItem, NGrid, NAvatar, NText, NFlex, NDropdown } from 'naive-ui';
 import ReadIcon from '../assets/read.svg';
 import UnreadIcon from '../assets/unread.svg';
-import { ref } from 'vue';
+import { nextTick, ref } from 'vue';
 
 const status = ref<boolean>(false);
 const message = ref<string>('last message');
 const username = ref<string>('User name');
 
+const showDropdownRef = ref(false)
+const xRef = ref(0)
+const yRef = ref(0)
+
+const activeDropdownId = ref<string | null>(null);
+
+function handleContextMenu(e: MouseEvent, id: string) {
+    e.preventDefault();
+    activeDropdownId.value = id;
+    showDropdownRef.value = false;
+    nextTick().then(() => {
+        showDropdownRef.value = true;
+        xRef.value = e.clientX;
+        yRef.value = e.clientY;
+    });
+}
+
+function onClickoutside() {
+    showDropdownRef.value = false;
+    activeDropdownId.value = null;
+}
+
+function handleSelect(key: string | number) {
+    showDropdownRef.value = false
+}
+
+
+const options = [
+    {
+        label: 'Jay Gatsby',
+        key: 'jay gatsby'
+    }
+]
+
 interface Props {
-  id: string;
+    id: string;
 }
 
 const props = defineProps<Props>();
@@ -17,7 +51,22 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-    <NFlex vertical class="m-l-5px w-250px h-50px bg-white hover-bg rounded-2 overflow-hidden p-l-10px p-r-10px">
+    <NFlex 
+        vertical 
+        class="m-l-5px w-250px h-50px bg-white rounded-2 overflow-hidden p-l-10px p-r-10px"
+        :class="{ 'hover-bg': !showDropdownRef, 'active-bg': activeDropdownId === props.id }" 
+        @contextmenu="(e) => handleContextMenu(e, props.id)"
+    >
+        <NDropdown
+            placement="bottom-start"
+            trigger="manual"
+            :x="xRef"
+            :y="yRef"
+            :options="options"
+            :show="showDropdownRef"
+            :on-clickoutside="onClickoutside"
+            @select="handleSelect"
+        />
         <NGrid :cols="5" :x-gap="5" class="h-50px">
             <NGridItem class="justify-center flex items-center">
                 <NAvatar 
@@ -55,6 +104,10 @@ const props = defineProps<Props>();
 }
 
 .hover-bg:hover {
+    background-color: #F4F7FF;
+}
+
+.active-bg {
     background-color: #F4F7FF;
 }
 </style>
