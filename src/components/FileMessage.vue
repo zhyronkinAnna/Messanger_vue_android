75a5fa2b78eadc0cee2e84387b36880530716ec9
dimaz <dimaz@DESKTOP-HGBFR2G>
2024-09-05 18:25:58 +0200

    fix

--- src/components/FileMessage.vue
+++ src/components/FileMessage.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { NButton, NFlex, NIcon, NText } from 'naive-ui';
+import { NButton, NDropdown, NFlex, NIcon, NText } from 'naive-ui';
 import UnreadIcon from '../assets/unread.svg';
 import ReadIcon from '../assets/read.svg';
 import { IChatMessage, ReadTypes } from '../models';
 import { useStore } from '../stores/store';
 import { DocumentTextIcon } from '@heroicons/vue/24/solid';
+import { nextTick, ref } from 'vue';
 
 const store = useStore();
 
@@ -25,16 +26,14 @@ function handleSelect(key: string | number) {
 }
 
 function handleContextMenu(e: MouseEvent, id: number) {
-    if(props.messageFile.username === store.user?.username){
-        e.preventDefault();
-        activeDropdownId.value = id;
-        showDropdownRef.value = false;
-        nextTick().then(() => {
-            showDropdownRef.value = true;
-            xRef.value = e.clientX;
-            yRef.value = e.clientY;
-        });
-    }
+    e.preventDefault();
+    activeDropdownId.value = id;
+    showDropdownRef.value = false;
+    nextTick().then(() => {
+        showDropdownRef.value = true;
+        xRef.value = e.clientX;
+        yRef.value = e.clientY;
+    });
 }
 
 function onClickoutside() {
@@ -50,7 +49,17 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-    <NFlex vertical :size="0" class="bg-#F4F4F7 p-10px" justify="center">
+    <NFlex vertical :size="0" class="bg-#F4F4F7 p-10px" justify="center" @contextmenu="(e) => handleContextMenu(e, props.messageFile.message_id)">
+        <NDropdown
+            placement="bottom-start"
+            trigger="manual"
+            :x="xRef"
+            :y="yRef"
+            :options="options"
+            :show="showDropdownRef"
+            :on-clickoutside="onClickoutside"
+            @select="handleSelect"
+        />
         <NFlex align="center">
             <NFlex align="center" justify="center" class="w-50px h-50px rounded-8px bg-#DCE8F5">
                 <NIcon :size="40"  color="#007AFFFF"><DocumentTextIcon/></NIcon>
