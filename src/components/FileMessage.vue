5f4b0baab72b297005da35a27e5999811f600864
Dmytro Tymenko <92209654+DmitryEvgenyevich@users.noreply.github.com>
2024-09-05 18:13:54 +0200

    Revert "add dropdown menu for messages"

--- src/components/FileMessage.vue
+++ src/components/FileMessage.vue
@@ -1,45 +1,12 @@
 <script setup lang="ts">
-import { NButton, NDropdown, NFlex, NIcon, NText } from 'naive-ui';
+import { NButton, NFlex, NIcon, NText } from 'naive-ui';
 import UnreadIcon from '../assets/unread.svg';
 import ReadIcon from '../assets/read.svg';
 import { IChatMessage, ReadTypes } from '../models';
 import { useStore } from '../stores/store';
 import { DocumentTextIcon } from '@heroicons/vue/24/solid';
-import { nextTick, ref } from 'vue';
 
 const store = useStore();
-
-const xRef = ref(0);
-const yRef = ref(0);
-const showDropdownRef = ref(false);
-const activeDropdownId = ref<number | null>(null);
-
-const options = [
-    {
-        label: 'Delete Message',
-        key: 'delete_message'
-    }
-]
-
-function handleSelect(key: string | number) {
-    showDropdownRef.value = false
-}
-
-function handleContextMenu(e: MouseEvent, id: number) {
-    e.preventDefault();
-    activeDropdownId.value = id;
-    showDropdownRef.value = false;
-    nextTick().then(() => {
-        showDropdownRef.value = true;
-        xRef.value = e.clientX;
-        yRef.value = e.clientY;
-    });
-}
-
-function onClickoutside() {
-    showDropdownRef.value = false;
-    activeDropdownId.value = null;
-}
 
 interface Props {
     messageFile: IChatMessage
@@ -49,17 +16,7 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-    <NFlex vertical :size="0" class="bg-#F4F4F7 p-10px" justify="center" @contextmenu="(e) => handleContextMenu(e, props.messageFile.message_id)">
-        <NDropdown
-            placement="bottom-start"
-            trigger="manual"
-            :x="xRef"
-            :y="yRef"
-            :options="options"
-            :show="showDropdownRef"
-            :on-clickoutside="onClickoutside"
-            @select="handleSelect"
-        />
+    <NFlex vertical :size="0" class="bg-#F4F4F7 p-10px" justify="center">
         <NFlex align="center">
             <NFlex align="center" justify="center" class="w-50px h-50px rounded-8px bg-#DCE8F5">
                 <NIcon :size="40"  color="#007AFFFF"><DocumentTextIcon/></NIcon>
